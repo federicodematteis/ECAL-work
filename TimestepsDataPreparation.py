4d1fa2e298dbd53a77232657a03efeb79a2b3825
federicodematteis <97627039+federicodematteis@users.noreply.github.com>
2022-01-29 23:17:24 +0100

    Add files via upload

---
+++ TimestepsDataPreparation.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+import numpy as np
+from pprint import pprint
+from collections import namedtuple
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--bril", help="Bril file", type=str)
+parser.add_argument("-t","--time-interval", help="Time interval in seconds", type=int)
+parser.add_argument("-o","--outputfile", help="Output file", type=str)
+args = parser.parse_args()
+
+# ## Read brilcalc metadata
+bril = pd.read_csv(args.bril, sep=",", comment="#")
+bril["run"] = bril.apply(lambda row: int(row["run:fill"].split(":")[0]), axis=1)
+bril["fill"] = bril.apply(lambda row: int(row["run:fill"].split(":")[1]), axis=1)
+
+
+bril.head()
+
+bril["lumi_in_fill"] = bril.groupby("fill")["delivered(/ub)"].cumsum()
+bril["lumi_int"] = bril["delivered(/ub)"].cumsum()
+bril["time_in_fill"] = bril.groupby("fill")["time"].transform(lambda t: t - t.min())
+bril["time_in_fill_stable"] = bril[bril.beamstatus=="STABLE BEAMS"].groupby("fill")["time"].transform(lambda t: t - t.min())
+bril["time_in_fill_stable"] = bril["time_in_fill_stable"].fillna(0)
+
+bril.head()
+
+
+# Useful Namestupls with fill information
+fillinfo = namedtuple("fillinfo", ["fill", "beamstatus", "lumi_inst", "lumi_int", "lumi_in_fill", "time_in_fill", "time_in_fill_stable"])
+
+
+def is_in_fill(timestamp):
+    a = bril[abs(bril.time - timestamp)<23]
+    if len(a):
+        a = a.iloc[0]
+        return fillinfo(a.fill, a.beamstatus, a["delivered(/ub)"], a.lumi_int, a.lumi_in_fill, a.time_in_fill, a.time_in_fill_stable)
+    else:
+        return fillinfo(0, "NOBEAM", 0, 0, 0,0,0)
+    
+#is_in_fill(starting_time)
+
+
+def get_lumi_interval(timestart, timestop):
+    return bril[ (bril.time >= timestart) & (bril.time <= timestop)]["delivered(/ub)"].sum()
+
+
+def get_last_fill_end(timestamp, fill=0):
+    df = bril[(bril.time <=timestamp) & (bril.fill != fill)]
+    if not df.empty:
+        return df.iloc[-1]
+    else: 
+        return pd.DataFrame()
+
+
+def get_fill_timeinterval(fill):
+    t = bril[bril.fill== fill].time
+    return t.iloc[0], t.iloc[-1]
+
+
+#get_fill_timeinterval(6417)
+
+
+# ## Lumi/fill metadata output
+# Let's read bril data to create metadata points every N minutes
+
+outputs = {
+    "in_fill" : [],
+    "time": [],
+    "lumi_inst": [],
+    "lumi_int" : [],
+    "lumi_in_fill": [],
+    "lumi_since_last_point": [],
+    "lumi_last_fill": [],
+    "fill_num": [],
+    "time_in_fill": [],
+    "time_in_fill_stable": [],
+    "time_from_last_fill" : [],
+    "last_dump_duration" : [],
+    "last_fill_duration": [],
+}
+
+
+def add_output(out):
+    for k, v in out.items():
+        outputs[k].append(v)
+
+
+# Interpolation time
+time_interval = args.time_interval
+
+
+previous_time = bril.time.iloc[0]
+last_int_lumi = 0.
+tot = (bril.time.iloc[-1]-bril.time.iloc[0])//time_interval
+
+for iev, curr_time in enumerate(range(bril.time.iloc[0], bril.time.iloc[-1], time_interval)):
+    if iev % 100 == 0:
+        print(f"{iev}/{tot}")
+    
+    fill_info = is_in_fill(curr_time)
+    if fill_info.fill != 0.:
+        last_int_lumi = fill_info.lumi_int
+    
+    last_fill_info = get_last_fill_end(curr_time, fill_info.fill)
+    
+    if last_fill_info.empty:
+        last_fill_end = curr_time
+        last_fill_duration = 0
+        last_dump_duration = 0
+        lumi_last_fill = 0
+    else:
+        last_fill_end = last_fill_info.time
+        last_fill_duration = last_fill_info.time_in_fill
+        lumi_last_fill = last_fill_info.lumi_in_fill
+        
+        if fill_info.fill != 0:
+            last_dump_duration =  (curr_time - fill_info.time_in_fill) - last_fill_info.time 
+        else:
+            last_dump_duration = curr_time - last_fill_info.time 
+    
+    time_from_last_fill = curr_time - last_fill_end
+    lumi_since_last_point = get_lumi_interval(previous_time,curr_time)
+    
+    out = {
+        "in_fill": int(fill_info.fill != 0),
+        "time": curr_time,
+        "fill_num": fill_info.fill,
+        "lumi_inst": fill_info.lumi_inst,
+        "lumi_int": last_int_lumi,
+        "lumi_in_fill": fill_info.lumi_in_fill,
+        "lumi_since_last_point": lumi_since_last_point,
+        "lumi_last_fill": lumi_last_fill,
+        
+        "time_in_fill": fill_info.time_in_fill,
+        "time_in_fill_stable":  fill_info.time_in_fill_stable,
+        "time_from_last_fill": time_from_last_fill, 
+        "last_dump_duration": last_dump_duration, 
+        "last_fill_duration": last_fill_duration
+    }
+    
+    previous_time = curr_time
+    add_output(out)
+
+output_df = pd.DataFrame(outputs)
+output_df.to_csv(args.outputfile, index=False, sep=",")
+
+print("Done")
---
+++ iRing.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pandas as pd
+import numpy as np
+from pprint import pprint
+from collections import namedtuple
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--transparency-metadata", help="Transparency metadata file", type=str)
+parser.add_argument("--timestamps-metadata", help="Timestamps metadata file", type=str)
+parser.add_argument("-o","--output-file", help="Output file", type=str)
+parser.add_argument("--iring", help="iring", type=int)
+parser.add_argument("-d","--data", help="Data folder")
+args = parser.parse_args()
+
+
+# In[ ]:
+
+
+data_EE = np.load(f"{args.data}/transp_data_EE_v2.npy", mmap_mode="r")
+data_EB = np.load(f"{args.data}/transp_data_EB_v2.npy", mmap_mode="r")
+
+
+# ## Read brilcalc metadata
+
+meta = pd.read_csv(args.transparency_metadata,sep=",").astype(
+            {"transp_entry":"int64", "time": "int64", "fill_num":"int64", "time_in_fill":"int64", "time_in_fill_stable":"int64"})
+
+
+timestamps_df = pd.read_csv(args.timestamps_metadata, sep=",", comment="#")
+
+
+# In[ ]:
+
+
+iring_df = pd.read_csv('https://raw.githubusercontent.com/amassiro/EcalPF/master/test/draw/eerings.dat', sep='\s', header=None, names=['ix', 'iy', 'iz', 'iring'], engine='python')
+
+
+# In[ ]:
+
+#restringe la lista a quella corrispondente a iring 1 (dato come argument) è un panda dataframe
+iring_df = iring_df[iring_df['iring'] == args.iring]
+
+
+# In[ ]:
+#definisce le funzioni con cui fa get 
+
+def get_transp_EB(index, x, y):
+    EB = data_EB[int(index)-1][x, y]
+    return EB
+
+def get_transp_EE(index, z, x, y):
+    EE = data_EE[int(index)-1][z][x, y]
+    return EE
+
+def get_transp_interval(timestamp):
+    last_meas = meta[(meta.time < timestamp)].iloc[-1]
+    next_meas = meta[(meta.time > timestamp)]
+    if next_meas.empty:
+        return last_meas, pd.DataFrame()
+    #print("{} {} | {}| x0: {} | Interval diff {:.3f}".format(last_meas.time, next_meas.time,  timestamp,timestamp- last_meas.time, ( next_meas.time - last_meas.time) / 60))
+    return last_meas, next_meas.iloc[0]
+
+#definisce le funzioni con cui intepola :
+
+#Trasparenza
+
+def interpolate_transp(x, x0, x1, y0, y1):
+    z = (x - x0)*( (y1-y0)/(x1-x0)) + y0
+    #print(f"x {x}, x0 {x0}, x1 {x1}, y0 {y0}, y1 {y1} ---> {z}")
+    return z
+
+
+def get_transp_interpolate(timestamp, x, y, z):
+    Z0, Z1 = get_transp_interval(timestamp)
+    if Z1.empty:
+        return -1.
+
+    if z == 0:
+        transp_EB_y0 = get_transp_EB(Z0.transp_entry, x, y)
+        transp_EB_y1 = get_transp_EB(Z1.transp_entry, x, y)
+        trans_EB = interpolate_transp(timestamp, Z0.time, Z1.time,  transp_EB_y0, transp_EB_y1)
+        return trans_EB
+    
+    if z == 1:
+        transp_EE_y0 = get_transp_EE(Z0.transp_entry, 0, x, y)
+        transp_EE_y1 = get_transp_EE(Z1.transp_entry, 0, x, y)
+        trans_EE =  interpolate_transp(timestamp,  Z0.time, Z1.time,  transp_EE_y0, transp_EE_y1)
+        return trans_EE
+
+    if z == -1:
+        transp_EE_y0 = get_transp_EE(Z0.transp_entry, 1, x, y)
+        transp_EE_y1 = get_transp_EE(Z1.transp_entry, 1, x, y)
+        trans_EE =  interpolate_transp(timestamp,  Z0.time, Z1.time,  transp_EE_y0, transp_EE_y1)
+        return trans_EE
+   
+    
+
+
+# In[ ]:
+
+#dichiara transp_output
+transp_output = []
+#dichiara tot
+tot = len(timestamps_df)
+    
+#main for: fa uno scan sui tempi dentro uno scan su len(iring_df.axes[0]) cioè sulla lunghezza delle righe di iring_df (quindi sulla sua dimensione!), quindi per ogni riga
+for i in range(0, len(iring_df.axes[0])):
+    print(f"{i}/{len(iring_df.axes[0])}")
+    #definisce tran per usarlo nel for sui tempi
+    tran = []
+    #scan sui tempi (per ogni riga) e per ogni t (quindi per tutto il tempo a disposizione in round da 10 minuti???)
+    for iev, row in timestamps_df.iterrows():
+        #if iev % 100 == 0:
+            #print(f"{iev}/{tot}")
+            #aggiunge una riga a tran riempiendola con il valore di T interpolata 
+            #T interpolata è calcolata in get_transp_interpolate con argomenti (time,iring_df.iloc[][])-1 
+            tran.append(get_transp_interpolate(row.time, (iring_df.iloc[i]['ix'])-1, (iring_df.iloc[i]['iy'])-1, iring_df.iloc[i]['iz']))
+    
+    transp_output.append(tran)
+    
+np.save(args.output_file,  np.array(transp_output))
+
+
+# In[ ]:
