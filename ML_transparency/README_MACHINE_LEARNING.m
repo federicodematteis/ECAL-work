ea5a7f31f36fb651980556d21e9dca3e5a3ab081
federicodematteis <97627039+federicodematteis@users.noreply.github.com>
2022-09-01 09:52:02 +0200

    Create README_MACHINE_LEARNING.m

---
+++ ML_transparency/README_MACHINE_LEARNING.m
@@ -0,0 +1,8 @@
+- Gli script nella cartella data_preparation sono costituiti da due funzioni, necessarie per compilare gli altri script nella cartella ML_transparency; serve a preparare dati di trasparenza (target function) 
+  e i metadati di Luminosità (features) per i modelli di machine learning usati nel lavoro.
+- Nella cartella di plotting sono contenuti alcuni script per plottare grafici della trasparenza per ogni i-Ring, altri grafici tili e la performance del trigger di CMS-ECAL
+  con e senza correzioni
+- Nella cartella betch_marking è contenuto uno script di draft per effettuare la comparazione di diversi modelli addestrati con iper-parametri diversi
+- Lo script Smooth_LHC_fills è fondamentale per studiare l'andamento della trasparenza all'interno di un singolo fill, per ogni fill nell' LHC RUN e per effettuare 
+  una selezione dei fill da usare nell'analisi.
+- Endcaps_mapping è un piccolo script il cui output è un grafico della pseudorapidità in funzione dell'angolo theta (vedere geometria di ECAL) che 'clustera' i cristalli in i-Rings concentrici.
