7dcf3f77cec505ca51bd9a5d04b631dfa7f2a214
federicodematteis <97627039+federicodematteis@users.noreply.github.com>
2022-09-01 09:42:02 +0200

    Create README.m

---
+++ ML_transparency/README.m
@@ -0,0 +1,37 @@
+# @brusale's Thesys 
+The aim of the work was to find a proper 2D-functional form for the Transparency of CMS-ECAL xtals (its correlation with Instantaneous luminosity and Integrated luminosity), and evaluate the consequences of its correction on the trigger efficiency.
+
+link to transparency repo: https://github.com/federicodematteis/Transparency
+
+# Data Preparation
+link to files: http://dvalsecc.web.cern.ch/dvalsecc/ECAL/Transparency/data_v1/ .
+
+1- LaserDataPreparation.py takes in input the file:
+BlueLaser_2017_rereco_v2_newformat.root.
+It outputs Transparency datas divided into two npy arrays, and several transparency metadata, with non-regular (non-constant) time-granularity: transp_metadata_2017_v2.csv, transp_data_EE.npy transp_data_EB.npy .
+
+2-TimestepsDataPreparation.py takes the lumi_brilcalc_2017.csv file and outputs several metadata with a 10 min time step: output_metadata_2017_10min.csv (or fill_metadata_2017_10min.csv)
+
+3-output_transp_timestamps.py takes in input:  
+transp_data_EE.npy, transp_data_EB.npy, transp_metadata_2017_v2.scv, output_metadata_2017_10min.csv.
+It interpolates the value of transparency with non regular and regular time-granularity datas, "generating" a npy array containing values for Transparency, which are now correspondents to the entries in the output_metadata_2017_10min.csv file (or fill_output_2017_10min).
+
+In his work @Brusale used iRing.py to do the same job of output_transp_timestamps.py, extracting values for transparency for all x,y,z in the selected iRing.
+
+# Plotting
+We will use only EE datas for now.
+Trading the path of @brusale I used the EE_2D_Fitting.py script for evaluating the parameters of fit_func2 on iRing<num>.npy's  mean Transparency entries and to plot several fit-function and bias.
+With the same script is possible to test the model on other iRings.
+Some scripts added for making predictions on transparency over a single fill.
+
+# Expected Performance
+Another script is used to convert iRing<num>.npy into .txt files: GetData.py, making them readable for the cxx script).
+@Brusale used TurnOnCurve.cxx for evaluating the effect of Transparency predictions on the trigger efficiency, which fills three different hist: -efficiency real, efficiency corrected and Ideal efficiency (the step function).
+ 
+The main focus was to have a starting point: TurnOnCurve for real Transparency datas, and a target: the step function, using parameters found with EE_2D_Fitting on some iRings
+
+# Machine Learning 
+The aim of the current work : Transparency predictions with a DNN regression.
+As a first step, before entering the machine learning environment, we go through 2D fitting, but now the fit is going to be on the single fill in order to make a more precise prediction on the entire run (in the previous work the perspective was to use just few iRing.npy file to get a higher trigger performance on the entire run, for what concerns 2D Fitting);
+ 
+(thanks to @brusale, @amassiro, @valsdav)
